5f33d5d57bbcbcc68b067c3ebb5b3f5f247b4bb2
shivani098787 <shivania1984@gmail.com>
2026-05-22 13:39:08 -0400

    commit

--- main.tf
+++ main.tf
@@ -19,16 +19,81 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
+
 #Attach Policy
 resource "aws_iam_role_policy_attachment" "ecr_policy" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
 }
 
+
 #Instance Profile
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "jenkins-instance-profile"
   role = aws_iam_role.ec2_role.name
+}
+
+
+
+# ECS Cluster
+resource "aws_ecs_cluster" "app_cluster" {
+  name = "sample-ecs-cluster"
+}
+
+
+# Creating a Security Group - Which Allows ALL Traffic
+resource "aws_security_group" "ec2_sg" {
+  name        = "allow-all-Traffic-sg"
+  description = "Allow all inbound and outbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"          
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" 
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-all-Traffic-sg"
+  }
+}
+
+
+
+
+# Get latest Amazon Linux AMI, What kind of AMI
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+
+
+# Creating sample EC2 Instance
+
+resource "aws_instance" "shivani_sample_ec2" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t2.micro"
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  tags = {
+    Name = "shivani_sample_ec2"
+  }
 }
 
 
